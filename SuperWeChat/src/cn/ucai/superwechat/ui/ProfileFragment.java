ed33e2c2d8a93452e5c269398afdc7daf43fe315
耿小泰 <clawpo@foxmail.com>
2017-03-31 11:49:23 +0800

    添加头像和昵称的数据提供方法,并显示

--- SuperWeChat/src/cn/ucai/superwechat/ui/ProfileFragment.java
+++ SuperWeChat/src/cn/ucai/superwechat/ui/ProfileFragment.java
@@ -6,17 +6,45 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.hyphenate.chat.EMClient;
+import com.hyphenate.easeui.utils.EaseUserUtils;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import cn.ucai.superwechat.R;
 
 /**
  * Created by clawpo on 2017/3/31.
  */
 public class ProfileFragment extends Fragment {
+    @BindView(R.id.iv_profile_avatar)
+    ImageView mIvProfileAvatar;
+    @BindView(R.id.tv_profile_nickname)
+    TextView mTvProfileNickname;
+    @BindView(R.id.tv_profile_username)
+    TextView mTvProfileUsername;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        ButterKnife.bind(this, view);
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initData();
+    }
+
+    private void initData() {
+        String username = EMClient.getInstance().getCurrentUser();
+        mTvProfileUsername.setText(username);
+        EaseUserUtils.setAppUserNick(username,mTvProfileNickname);
+        EaseUserUtils.setAppUserAvatar(getContext(),username,mIvProfileAvatar);
     }
 }
