aeb1bdb1f383694976d72b74c07ffad6c5bc1c01
耿小泰 <clawpo@foxmail.com>
2017-04-01 11:36:51 +0800

    跳转到个人资料不需要参数

--- SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
+++ SuperWeChat/src/cn/ucai/superwechat/utils/MFGT.java
@@ -58,9 +58,7 @@ public class MFGT {
         startActivity(activity,SettingsActivity.class);
     }
 
-    public static void gotoUserInfo(Activity activity, boolean settings, String username) {
-        startActivity(activity,new Intent(activity, UserProfileActivity.class)
-                .putExtra("setting", settings)
-                .putExtra("username", username));
+    public static void gotoUserInfo(Activity activity) {
+        startActivity(activity,UserProfileActivity.class);
     }
 }
