5c89a5241715a9ca45e90ee9b7ef9c83d399e911
耿小泰 <clawpo@foxmail.com>
2017-04-06 16:10:09 +0800

    添加好友到数据库已经内存中,并刷新联系人列表

--- SuperWeChat/src/cn/ucai/superwechat/SuperWeChatHelper.java
+++ SuperWeChat/src/cn/ucai/superwechat/SuperWeChatHelper.java
@@ -741,7 +741,7 @@ public class SuperWeChatHelper {
             broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
         }
 
-        private void onAppContactAdded(String username) {
+        private void onAppContactAdded(final String username) {
             userModel.addContact(appContext, EMClient.getInstance().getCurrentUser(), username,
                     new OnCompleteListener<String>() {
                         @Override
@@ -751,9 +751,15 @@ public class SuperWeChatHelper {
                                 if (result!=null && result.isRetMsg()){
                                     User u = (User) result.getRetData();
                                     if (u!=null){
-                                        //保存到内存
                                         //保存到数据库
+                                        Map<String, User> appContactList = getAppContactList();
+                                        if (!appContactList.containsKey(username)){
+                                            userDao.saveAppContact(u);
+                                        }
+                                        //保存到内存
+                                        appContactList.put(u.getMUserName(),u);
                                         //通知联系人列表更新
+                                        broadcastManager.sendBroadcast(new Intent(Constant.ACTION_CONTACT_CHANAGED));
                                     }
                                 }
                             }
